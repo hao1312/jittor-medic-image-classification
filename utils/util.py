a71e9bc0009c3b8728ae471473757b383cb89fdf
hao1312 <97863180+hao1312@users.noreply.github.com>
2025-09-04 15:40:41 +0800

    Delete utils directory

--- utils/util.py
+++
@@ -1,9 +0,0 @@
-
-def update_values(dict_from, dict_to):
-    for key, value in dict_from.items():
-        if isinstance(value, dict):
-            update_values(dict_from[key], dict_to[key])
-        elif value is not None:
-            dict_to[key] = dict_from[key]
-
-
